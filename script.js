a48fe5af2f08e5515e660f945950d5336f094e99
sagor-khan7 <sksagor791@gmail.com>
2024-12-27 09:40:12 +0600

    Implemented functionality for open and close modal

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log("clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+overlay.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
